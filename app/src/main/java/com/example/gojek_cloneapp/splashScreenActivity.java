5924199a2600a53b87fd7906b412ee0a3bb3670c
rasyidk <rasid.rasid9961@gmail.com>
2022-11-11 15:19:19 +0700

    .

---
+++ app/src/main/java/com/example/gojek_cloneapp/splashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.gojek_cloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        Timer t =new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+
+                    Intent i =  new Intent(splashScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+
+
+
+            }
+        },2000);
+
+    }
+}
